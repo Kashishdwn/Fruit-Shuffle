bfdb875621cce54b5473df0d988a38ee279dc7d1
kashish19 <kashishtheboy@gmail.com>
2020-04-11 01:01:46 +0530

    Updates

---
+++ precache-manifest.ff10138a6c6db619af9da8093f84213f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79883a8ea69323a011a19261e236e0a0",
+    "url": "/Fruit-Shuffle/index.html"
+  },
+  {
+    "revision": "1414920a3facbfb78696",
+    "url": "/Fruit-Shuffle/static/js/2.465baff5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fruit-Shuffle/static/js/2.465baff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b701224443d81694d24d",
+    "url": "/Fruit-Shuffle/static/js/main.7d1aa60e.chunk.js"
+  },
+  {
+    "revision": "f7f69f6efeff6ce0d884",
+    "url": "/Fruit-Shuffle/static/js/runtime-main.5c251c08.js"
+  }
+]);
